36ca213ce2e65f58f4e3f229f81a0ba40c842fc6
shivam6862 <shivam6862mau@gmail.com>
2023-11-05 23:13:32 +0530

    notification

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -2,6 +2,8 @@ import "../styles/globals.css";
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 import Header from "@/components/Header";
+import { NotificationContextProvider } from "@/store/notification/Notification-context";
+import Notifications from "@/components/notification/Notifications";
 
 export const metadata = {
   title: "Fault Tracker",
@@ -13,8 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <NotificationContextProvider>
+          <Header />
+          {children}
+          <Notifications />
+        </NotificationContextProvider>
       </body>
     </html>
   );
